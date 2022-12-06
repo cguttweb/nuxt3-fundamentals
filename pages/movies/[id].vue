ed2c52ca61810fb128d2fa9d68839966d53cff55
Chloe <cguttridge@clever-touch.com>
2022-12-06 19:22:52 +0000

    added dynamic movie page & how to $fetch for API calls

---
+++ pages/movies/[id].vue
@@ -0,0 +1,10 @@
+<script setup>
+const route = useRoute()
+const msg = computed(() => `My route id is ${route.params.id}`)
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,7 +1,30 @@
-<script lang="ts" setup></script>
+<script setup>
+const query = ref('')
+const movies = ref([])
+
+async function search() {
+  // destructure search array result from request
+  const { Search } = await $fetch(
+    `http://www.omdbapi.com/?apikey=3726efc0&s=${query.value}`
+  )
+  movies.value = Search
+  console.log(movies)
+}
+</script>
 
 <template>
-  <div>Page: Movies</div>
+  <form @submit.prevent="search">
+    <input type="text" name="" id="" v-model="query" />
+    <button>Search</button>
+  </form>
+
+  <ul style="display: flex; flex-wrap: wrap; gap: 10px; list-style: none">
+    <li v-for="movie in movies" :key="movie.imdbID">
+      <NuxtLink :to="{ name: 'movies-id', params: { id: movie.imdbID } }">
+        <img :src="movie.Poster" :alt="movie.title" />
+      </NuxtLink>
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
