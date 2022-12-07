f1f0594ed3bbb681454aed8ab7dbc8c649970505
Chloe <cguttweb@pm.me>
2022-12-07 17:27:34 +0000

    learning about some useAsyncData options

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,8 +1,23 @@
 <script setup>
 const route = useRoute()
-const { data } = await useAsyncData(() => {
-  return $fetch(`http://www.omdbapi.com/?apikey=3726efc0&i=${route.params.id}`)
-})
+const { data } = await useAsyncData(
+  `/movies/${route.params.id}`,
+  () => {
+    return $fetch(
+      `http://www.omdbapi.com/?apikey=3726efc0&i=${route.params.id}`
+    )
+  },
+  {
+    // limit options for keys
+    pick: ['Plot', 'Title'],
+    transform(data) {
+      return {
+        Plot: data.Plot,
+        Title: data.Title,
+      }
+    },
+  }
+)
 </script>
 
 <template>
