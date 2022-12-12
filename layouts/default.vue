d3270bca5378d8b4371a4a2e0fde76cd3d359a2f
Chloe <cguttweb@pm.me>
2022-12-12 19:07:00 +0000

    learning about composables and global state management with useState composable

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,16 @@
+<script setup>
+const isLoggedIn = useIsLoggedIn()
+</script>
+
 <template>
   <div>
     <nav style="display: flex; gap: 15px">
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/movies">Movies</NuxtLink>
       <NuxtLink to="https://vueschool.io">Vue School</NuxtLink>
+      <NuxtLink v-if="!isLoggedIn" to="/login">Login</NuxtLink>
+      <a href="#" v-else @click.prevent="isLoggedIn = false">Logout</a>
     </nav>
     <slot></slot>
   </div>
 </template>
-
-<script setup></script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,21 +1,30 @@
-<template>
-  <div>
-  <h2>Login</h2>
-    <form>
-      <label for="">
-        Username
-        <input type="text" name="" id="" />
-      </label>
-      <label for="">
-        Password
-        <input type="password" name="" id="" />
-      </label>
-    </form>
-  </div>
-</template>
-
 <script setup>
+// 1st arg = key to identity the state & 2nd arg is default needs to be returned from callback function
+// const isLoggedIn = useState('isLoggedIn', () => false)
+const isLoggedIn = useIsLoggedIn()
 definePageMeta({
   layout: 'plain',
 })
+
+function login() {
+  isLoggedIn.value = true
+  useRouter().push('/')
+}
 </script>
+
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <label for="">
+        Username
+        <input type="text" />
+      </label>
+      <label for="">
+        Password
+        <input type="password" autocomplete="test" />
+      </label>
+      <button>Login</button>
+    </form>
+  </div>
+</template>
