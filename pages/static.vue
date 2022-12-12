f07555f7380843ea69bc2a42a668a81d1076937c
Chloe <cguttweb@pm.me>
2022-12-12 20:20:48 +0000

    created some test pages for different rendering modes

---
+++ pages/static.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Static Page</h1>
+    <h2>{{ new Date() }}</h2>
+  </div>
+</template>
---
+++ pages/swr.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>SWR Page</h1>
+  <h2>{{ new Date() }}</h2>
+</template>
